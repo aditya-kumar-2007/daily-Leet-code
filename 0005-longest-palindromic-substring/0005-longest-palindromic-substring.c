79fd3527dc69d4e379475b069f3f29dbeda44953
aditya-kumar-2007 <adityakumarsingh442007@gmail.com>
2026-07-16 21:58:11 +0530

    Time: 6 ms (75.16%), Space: 9 MB (75.57%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,35 @@
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+    if (n == 0) return "";
+
+    int start = 0, maxLen = 1;
+
+    for (int i = 0; i < n; i++) {
+        int l = i, r = i;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
+            }
+            l--;
+            r++;
+        }
+
+        l = i;
+        r = i + 1;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
+            }
+            l--;
+            r++;
+        }
+    }
+
+    char *ans = (char *)malloc(maxLen + 1);
+    strncpy(ans, s + start, maxLen);
+    ans[maxLen] = '\0';
+
+    return ans;
+}

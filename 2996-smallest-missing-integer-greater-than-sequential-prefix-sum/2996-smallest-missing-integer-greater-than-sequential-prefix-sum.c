a12aad2539c49e29d5fb685e557929b0a3a4504a
aditya-kumar-2007 <adityakumarsingh442007@gmail.com>
2026-08-11 22:07:43 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.27%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.c
@@ -0,0 +1,10 @@
+int missingInteger(int* a, int n) {
+    int s = a[0], i, j;
+    for(i=1;i<n && a[i]==a[i-1]+1;i++) s+=a[i];
+
+    for(; ;s++){
+        for(j=0;j<n;j++)
+            if(a[j]==s) break;
+        if(j==n) return s;
+    }
+}

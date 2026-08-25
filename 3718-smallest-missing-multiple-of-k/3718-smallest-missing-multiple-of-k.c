e63b6b89fdb977cdf67479f585daaf63a8631b91
aditya-kumar-2007 <adityakumarsingh442007@gmail.com>
2026-08-25 22:35:49 +0530

    Time: 0 ms (100%), Space: 9.8 MB (39.47%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.c
@@ -0,0 +1,19 @@
+int missingMultiple(int* nums, int numsSize, int k) {
+    long long x=k;
+    while(1){
+       int found = 0;
+        for(int i = 0; i < numsSize; i++){
+            if(nums[i] == x){
+                found = 1;
+                break;
+
+            }
+        }
+        if (!found)
+            return x;
+        x += k;
+
+    }
+    
+    
+}

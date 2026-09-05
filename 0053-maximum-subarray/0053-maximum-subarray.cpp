9e89fce7e408ad8ef1134798fd6bb0b40dfa825b
aditya-kumar-2007 <adityakumarsingh442007@gmail.com>
2026-09-05 22:25:35 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_current = nums[0];
+        int max_globle = nums[0];
+        
+        for(size_t i = 1; i < nums.size(); ++i){
+            max_current = std::max(nums[i], max_current + nums[i]);
+            if(max_current > max_globle){
+                max_globle = max_current;
+            }
+        }
+        return max_globle;
+    }
+};
